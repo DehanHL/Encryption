af2edbbde4f6d3379ed31aa909720b8632dd3a80
DEHAN LENTING <34535802@student365.msfed.nwu.ac.za>
2021-05-20 12:13:25 +0200

    Add project files.

---
+++ Encryption/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿
+namespace Encryption
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblPath = new System.Windows.Forms.Label();
+            this.tbxPath = new System.Windows.Forms.TextBox();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.btnExec = new System.Windows.Forms.Button();
+            this.lblPass = new System.Windows.Forms.Label();
+            this.tbxPassword = new System.Windows.Forms.TextBox();
+            this.rbEncrypt = new System.Windows.Forms.RadioButton();
+            this.rbDecrypt = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // lblPath
+            // 
+            this.lblPath.AutoSize = true;
+            this.lblPath.Location = new System.Drawing.Point(12, 43);
+            this.lblPath.Name = "lblPath";
+            this.lblPath.Size = new System.Drawing.Size(55, 15);
+            this.lblPath.TabIndex = 0;
+            this.lblPath.Text = "File path:";
+            // 
+            // tbxPath
+            // 
+            this.tbxPath.Location = new System.Drawing.Point(12, 61);
+            this.tbxPath.Name = "tbxPath";
+            this.tbxPath.Size = new System.Drawing.Size(267, 23);
+            this.tbxPath.TabIndex = 1;
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(285, 61);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowse.TabIndex = 2;
+            this.btnBrowse.Text = "Browse";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // btnExec
+            // 
+            this.btnExec.Location = new System.Drawing.Point(12, 125);
+            this.btnExec.Name = "btnExec";
+            this.btnExec.Size = new System.Drawing.Size(348, 23);
+            this.btnExec.TabIndex = 3;
+            this.btnExec.Text = "Start";
+            this.btnExec.UseVisualStyleBackColor = true;
+            this.btnExec.Click += new System.EventHandler(this.btnExec_Click);
+            // 
+            // lblPass
+            // 
+            this.lblPass.AutoSize = true;
+            this.lblPass.Location = new System.Drawing.Point(12, 9);
+            this.lblPass.Name = "lblPass";
+            this.lblPass.Size = new System.Drawing.Size(60, 15);
+            this.lblPass.TabIndex = 4;
+            this.lblPass.Text = "Password:";
+            // 
+            // tbxPassword
+            // 
+            this.tbxPassword.Location = new System.Drawing.Point(78, 6);
+            this.tbxPassword.Name = "tbxPassword";
+            this.tbxPassword.PasswordChar = '*';
+            this.tbxPassword.Size = new System.Drawing.Size(201, 23);
+            this.tbxPassword.TabIndex = 5;
+            // 
+            // rbEncrypt
+            // 
+            this.rbEncrypt.AutoSize = true;
+            this.rbEncrypt.Location = new System.Drawing.Point(12, 100);
+            this.rbEncrypt.Name = "rbEncrypt";
+            this.rbEncrypt.Size = new System.Drawing.Size(65, 19);
+            this.rbEncrypt.TabIndex = 6;
+            this.rbEncrypt.TabStop = true;
+            this.rbEncrypt.Text = "Encrypt";
+            this.rbEncrypt.UseVisualStyleBackColor = true;
+            this.rbEncrypt.CheckedChanged += new System.EventHandler(this.rbEncrypt_CheckedChanged);
+            // 
+            // rbDecrypt
+            // 
+            this.rbDecrypt.AutoSize = true;
+            this.rbDecrypt.Location = new System.Drawing.Point(112, 100);
+            this.rbDecrypt.Name = "rbDecrypt";
+            this.rbDecrypt.Size = new System.Drawing.Size(66, 19);
+            this.rbDecrypt.TabIndex = 7;
+            this.rbDecrypt.TabStop = true;
+            this.rbDecrypt.Text = "Decrypt";
+            this.rbDecrypt.UseVisualStyleBackColor = true;
+            this.rbDecrypt.CheckedChanged += new System.EventHandler(this.rbDecrypt_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.rbDecrypt);
+            this.Controls.Add(this.rbEncrypt);
+            this.Controls.Add(this.tbxPassword);
+            this.Controls.Add(this.lblPass);
+            this.Controls.Add(this.btnExec);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.tbxPath);
+            this.Controls.Add(this.lblPath);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblPath;
+        private System.Windows.Forms.TextBox tbxPath;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.Button btnExec;
+        private System.Windows.Forms.Label lblPass;
+        private System.Windows.Forms.TextBox tbxPassword;
+        private System.Windows.Forms.RadioButton rbEncrypt;
+        private System.Windows.Forms.RadioButton rbDecrypt;
+    }
+}
+
---
+++ Encryption/Form1.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Encryption
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //AES Global Variables
+        CspParameters cssp = new CspParameters();
+        RSACryptoServiceProvider rsa;
+
+        //Method to generate SHA256 hash from string
+        private byte[] getHash(string pass)
+        {
+            SHA256 sha256 = SHA256.Create();
+            byte[] hash = sha256.ComputeHash(Encoding.UTF8.GetBytes(pass));
+            return hash;
+        }
+
+        //Encrypt with cipher
+        private byte[] CipherEnc(byte[] inByte)
+        {
+            byte[] abc = getHash(tbxPassword.Text);
+            byte[] result = new byte[inByte.Length];
+            int index = 0;
+
+
+            for (int i = 0; i < inByte.Length; i++)
+            {
+                index = index % abc.Length;
+                int shift = (int)abc[index] - 65;
+                result[i] = (byte)(((int)inByte[i] + shift) % 256);
+                index++;
+
+                if (i == 256)
+                {
+                    index = 0;                    
+                }
+            }
+            return result;
+        }
+
+        //Decrypt with cipher
+        private byte[] CipherDec(byte[] inByte)
+        {
+            byte[] abc = getHash(tbxPassword.Text);
+            byte[] result = new byte[inByte.Length];
+            int index = 0;
+
+            for (int i = 0; i < inByte.Length; i++)
+            {
+                index = index % abc.Length;
+                int shift = (int)abc[index] - 65;
+                result[i] = (byte)(((int)inByte[i] + 256 - shift) % 256);
+                index++;
+
+                if (i == 256)
+                {
+                    index = 0;
+                }
+            }
+            return result;
+        }
+
+       /* public byte[] AesEnc(byte[] inByte)
+        {
+            byte[] result = new byte[inByte.Length];
+
+            Aes aes = Aes.Create();
+            ICryptoTransform cTrans = aes.CreateEncryptor();
+
+            byte[] eKey = RSA.Encrypt(aes.Key, false);
+
+            return result;
+        }*/
+
+        //Save Method
+        private void SaveFile(byte[] inByte)
+        {
+            string ext = Path.GetExtension(tbxPath.Text);
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "Files (*" + ext + ") | *" + ext;
+            if(sfd.ShowDialog() == DialogResult.OK)
+            {
+                File.WriteAllBytes(sfd.FileName, inByte);                
+            }
+        }
+        
+
+        //Select file
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Multiselect = false;
+            if(ofd.ShowDialog() == DialogResult.OK)
+            {
+                tbxPath.Text = ofd.FileName;
+            }
+        }
+
+        private void rbEncrypt_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rbEncrypt.Checked)
+            {
+                rbDecrypt.Checked = false;
+            }
+        }
+
+        private void rbDecrypt_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rbDecrypt.Checked)
+            {
+                rbEncrypt.Checked = false;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            rbEncrypt.Checked = true;
+        }
+
+        private void btnExec_Click(object sender, EventArgs e)
+        {
+            //Check rb
+            if (!File.Exists(tbxPath.Text))
+            {
+                MessageBox.Show("File does not exist.");
+            }
+            if(String.IsNullOrEmpty(tbxPassword.Text))
+            {
+                MessageBox.Show("Password empty. Please enter your password");
+                return;
+            }
+
+            try
+            {
+                byte[] FileByte = File.ReadAllBytes(tbxPath.Text);
+                //Encrypt
+                if (rbEncrypt.Checked)
+                {                    
+                    byte[] chipherByte = CipherEnc(FileByte);
+                    SaveFile(chipherByte);
+                    MessageBox.Show("File has been succesfully encrypted");
+                }
+                //Decrypt
+                else
+                {
+                    byte[] chipherByte = CipherDec(FileByte);
+                    SaveFile(chipherByte);
+                    MessageBox.Show("File has been succesfully decrypted");
+                }
+            }
+            catch
+            {
+                MessageBox.Show("File is in use");
+                return;
+            }
+            
+        }
+    }
+}
